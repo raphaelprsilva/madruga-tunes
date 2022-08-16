445bc0bc83fb6a662626d946a6dda6c163765ac2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-16 15:13:46 -0300

    feat: add routes to app pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import {
+  Login,
+  Search,
+  Album,
+  Favorites,
+  Profile,
+  ProfileEdit,
+  NotFound,
+} from './pages';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/album/:id"
+            render={ (props) => <Album { ...props } /> }
+          />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
