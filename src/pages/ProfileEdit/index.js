9ae60cc19f8f5eb7e1b3712bbb010ca213894fa6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-16 15:12:25 -0300

    feat: implements ProfileEdit page

---
+++ src/pages/ProfileEdit/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return <div data-testid="page-profile-edit">Page Profile Edit</div>;
+  }
+}
+
+export default ProfileEdit;
