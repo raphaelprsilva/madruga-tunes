8aa2882c17458e9110aae20a160013cf738a4c2b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-30 13:41:32 -0300

    feat: ensure musics album are save as favorites

--- src/components/MusicCard/index.js
+++ src/components/MusicCard/index.js
@@ -1,22 +1,71 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+import { addSong } from '../../services/favoriteSongsAPI';
+import Loading from '../Loading';
+
 import * as S from './styled';
 
 class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      inputFavoriteSong: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async handleChange({ target }) {
+    const { name } = target;
+    const { song } = this.props;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      loading: true,
+      [name]: value,
+    });
+
+    await addSong(song.trackId);
+    this.setState({ loading: false });
+  }
+
   render() {
     const { song } = this.props;
+    const { inputFavoriteSong, loading } = this.state;
+
     return (
       <S.MusicsWrapper key={ song.trackId }>
-        <p>{song.trackName}</p>
-        <div>
-          <audio data-testid="audio-component" src={ song.previewUrl } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            <code>audio</code>
-            .
-          </audio>
-        </div>
+        {
+          loading ? <Loading /> : (
+            <>
+              <p>{song.trackName}</p>
+              <div>
+                <audio data-testid="audio-component" src={ song.previewUrl } controls>
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  <code>audio</code>
+                  .
+                </audio>
+              </div>
+              <div>
+                <label htmlFor={ song.trackId }>
+                  Favorita
+                  <input
+                    onChange={ this.handleChange }
+                    checked={ inputFavoriteSong }
+                    data-testid={ `checkbox-music-${song.trackId}` }
+                    type="checkbox"
+                    name="inputFavoriteSong"
+                    id={ song.trackId }
+                  />
+                </label>
+              </div>
+            </>
+          )
+        }
       </S.MusicsWrapper>
     );
   }
