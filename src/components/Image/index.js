abe7a57941b743c6e3179ccc1aa366d751dbf391
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-09-01 19:18:42 -0300

    refactor: add defaultProps

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -8,9 +8,14 @@ class Image extends Component {
   }
 }
 
+Image.defaultProps = {
+  imageAlt: '',
+  imageSrc: '',
+};
+
 Image.propTypes = {
-  imageAlt: PropTypes.string.isRequired,
-  imageSrc: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string,
+  imageSrc: PropTypes.string,
 };
 
 export default Image;
